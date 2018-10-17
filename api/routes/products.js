1fc063b9d11aa5ef64bb6dde1603bfa14560c2bf
harukanoo <harukanox2@gmail.com>
2018-10-17 19:23:40 +0700

    connecting to nosql database

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
 
 router.get('/',(req,res,next) =>{
     res.status(200).json({
@@ -9,11 +12,21 @@ router.get('/',(req,res,next) =>{
 
 router.post('/',(req,res,next)=>{
 
-    const product ={
-        productId:req.body.productId,
+    // const product ={
+    //     productId:req.body.productId,
+    //     name:req.body.name,
+    //     price:req.body.price
+    // }
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name:req.body.name,
-        price:req.body.price
-    }
+        price :req.body.price
+    });
+
+    product.save().then(result=>{
+        console.log(result);
+    });
 
     res.status(200).json({
         message:'product has been saved',
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb://harukanoo:harukanoo2@rest-shop-shard-00-00-jxtdl.mongodb.net:27017,rest-shop-shard-00-01-jxtdl.mongodb.net:27017,rest-shop-shard-00-02-jxtdl.mongodb.net:27017/test?ssl=true&replicaSet=rest-shop-shard-0&authSource=admin&retryWrites=true',{
+    useMongoClient:true
+})
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
