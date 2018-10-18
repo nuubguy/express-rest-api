cde116516a92ddf75cd0fc7ded5caebfd602246f
THOMAS EDISSON RUNKAT <thomas.runkat@binus.ac.id>
2018-10-18 16:47:30 +0700

    creating get and post for product

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,19 +5,27 @@ const Product = require('../models/product');
 
 
 router.get('/',(req,res,next) =>{
-    res.status(200).json({
-        message:'product its ok'
-    })    
+    Product.find().exec().then(doc =>{
+        res.status(200).json(doc);
+    }).catch(err=>{
+        res.status(500).json({error:err});
+    });    
+});
+
+router.get('/:productId',(req,res,next) =>{
+    const id = req.params.productId;
+    Product.findById(id).exec().then(doc =>{    
+        if(doc==null){
+            res.status(404).json({message:"product not found"});
+        }else{
+            res.status(200).json(doc);
+        }
+    }).catch(err=>{
+        res.status(500).json({error:err});
+    });    
 });
 
 router.post('/',(req,res,next)=>{
-
-    // const product ={
-    //     productId:req.body.productId,
-    //     name:req.body.name,
-    //     price:req.body.price
-    // }
-
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name:req.body.name,
